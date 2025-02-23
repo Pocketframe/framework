c7bea332849b5c324e67f047162e062dee900ce1
William Asaba <asabawilliamdk@yahoo.com>
2025-02-23 21:09:30 +0300

    wip

--- src/Logger/Logger.php
+++ src/Logger/Logger.php
@@ -2,14 +2,18 @@
 
 namespace Pocketframe\Logger;
 
+use Exception;
 use Pocketframe\Contracts\LoggerInterface;
 
 class Logger implements LoggerInterface
 {
   public function log($message): void
   {
+    $exception = new Exception($message);
     $date = date('Y-m-d H:i:s');
-    $formattedMessage = "[$date] $message" . PHP_EOL;
+
+    $formattedMessage = "[{$date}] [ERROR] {$exception->getMessage()} in {$exception->getFile()}:{$exception->getLine()}\n{$exception->getTraceAsString()}\n";
+
     file_put_contents(base_path('store/logs/pocketframe.log'), $formattedMessage, FILE_APPEND);
   }
 }
