8258b9a116eb81a7e16dd24120aa485ea42dc409
William Asaba <asabawilliamdk@yahoo.com>
2025-03-03 09:38:09 +0300

    refactor: modify the constructor

--- src/Exceptions/Database/DatabaseException.php
+++ src/Exceptions/Database/DatabaseException.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Pocketframe\Exceptions;
 
-use Exception;
-
 class DatabaseException extends PocketframeException
 {
-    public function __construct(string $message, int $code = 500, string $errorType = 'database', ?Exception $previous = null)
+    public function __construct($message = "Failed to connect to the database")
     {
-        parent::__construct($message, $code, 'Database', $previous);
+        parent::__construct($message, 500);
     }
 }
