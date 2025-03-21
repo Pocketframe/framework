264a1368db3835848226a47ee9d2e9eae2040d3f
William Asaba <asabawilliamdk@yahoo.com>
2025-03-21 07:49:36 +0300

    Add MassAssignment error handler

---
+++ src/PocketORM/Exceptions/MassAssignmentError.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Pocketframe\PocketORM\Exceptions;
+
+final class MassAssignmentError extends ModelException
+{
+  public function __construct(string $modelClass, string $invalidKey)
+  {
+    parent::__construct(
+      "Mass assignment blocked for '{$invalidKey}' on {$modelClass}",
+      self::MASS_ASSIGNMENT_ERROR,
+      $modelClass,
+      ['invalid_key' => $invalidKey]
+    );
+  }
+}
