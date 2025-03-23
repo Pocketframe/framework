3ed3da8e235bf274701b570fd7ce311327d124a7
William Asaba <asabawilliamdk@yahoo.com>
2025-03-23 23:47:34 +0300

    wip: this is work ongoing

--- src/Exceptions/Database/DatabaseException.php
+++ src/Exceptions/Database/DatabaseException.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace Pocketframe\Exceptions;
 
-class DatabaseException extends PocketframeException
+use PDOException;
+use RuntimeException;
+
+class DatabaseException extends PDOException implements \Throwable
 {
-    public function __construct($message = "Failed to connect to the database")
-    {
-        parent::__construct($message, 500);
-    }
+  public function __construct($message = "Failed to connect to the database")
+  {
+    parent::__construct(
+      $message
+    );
+  }
 }
