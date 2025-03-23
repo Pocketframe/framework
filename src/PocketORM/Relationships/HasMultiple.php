fe94873cd085e0b826cad4864ef59854968b3833
William Asaba <asabawilliamdk@yahoo.com>
2025-03-23 23:54:10 +0300

    enhance: - improve types - add more methods

--- src/PocketORM/Relationships/HasMultiple.php
+++ src/PocketORM/Relationships/HasMultiple.php
@@ -3,25 +3,50 @@
 namespace Pocketframe\PocketORM\Relationships;
 
 use Pocketframe\PocketORM\Database\QueryEngine;
-use Pocketframe\PocketORM\Essentials\RecordSet;
+use Pocketframe\PocketORM\Essentials\DataSet;
+use Pocketframe\PocketORM\Entity\Entity;
 
-// same as has many
+/**
+ * HasMultiple: one-to-many relationship (a parent has multiple children).
+ */
 class HasMultiple
 {
-  private $parent;
-  private $related;
-  private $foreignKey;
+  private Entity $parent;
+  private string $related;
+  private string $foreignKey;
 
-  public function __construct($parent, string $related, string $foreignKey)
+  public function __construct(Entity $parent, string $related, string $foreignKey)
   {
-    $this->parent = $parent;
-    $this->related = $related;
+    $this->parent     = $parent;
+    $this->related    = $related;
     $this->foreignKey = $foreignKey;
   }
 
-  public function get(): RecordSet
+  public function eagerLoad(array $parents): array
   {
-    return (new QueryEngine($this->related::getTable()))
+    $parentIds = array_column(array_map(fn($p) => (array)$p, $parents), 'id');
+
+    $relatedRecords = (new QueryEngine($this->related::getTable(), $this->related::class))
+      ->whereIn($this->foreignKey, $parentIds)
+      ->groupBy($this->foreignKey)
+      ->get();
+
+    return $relatedRecords->all();
+  }
+
+  public function getForeignKey(): string
+  {
+    return $this->foreignKey;
+  }
+
+  public function getParentKey(): string
+  {
+    return 'id';
+  }
+
+  public function get(): DataSet
+  {
+    return (new QueryEngine($this->related::getTable(), $this->related::class))
       ->where($this->foreignKey, '=', $this->parent->id)
       ->get();
   }
